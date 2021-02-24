6c0a3291f145785bce7db2e3043e5e6e9b672c06
antsunzj <435042834@qq.com>
2021-02-24 13:28:49 +0800

    Update miot.sh

--- miot.sh
+++ miot.sh
@@ -4,3 +4,8 @@ wget https://github.com/ha0y/xiaomi_miot_raw/archive/master.zip
 unzip master.zip
 cp -rf /root/miot/xiaomi_miot_raw-master/custom_components/xiaomi_miot_raw /root/homeassistant/config01/custom_components/
 docker restart home-assistant
+cd /root/miot
+rm -rf master.zip
+rm -rf xiaomi_miot_raw-master
+
+
