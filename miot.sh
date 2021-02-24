31eab08d88ad3f01036f04ee14fbf5f3998e91ea
antsunzj <435042834@qq.com>
2021-02-24 13:13:04 +0800

    Add files via upload

---
+++ miot.sh
@@ -0,0 +1,6 @@
+mkdir miot
+cd /root/miot
+wget https://github.com/ha0y/xiaomi_miot_raw/archive/master.zip
+unzip master.zip
+cp -rf /root/miot/xiaomi_miot_raw-master/custom_components/xiaomi_miot_raw /root/homeassistant/config01/custom_components/
+docker restart home-assistant
